b5ece5461f8af484ebf82f291c437315cc92faa7
LordMeelo817 <131396490+LordMeelo817@users.noreply.github.com>
2026-04-20 17:31:21 -0500

    lms courses

--- addons/lms_course_management/__manifest__.py
+++ addons/lms_course_management/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'LMS Course Management',  
     'version': '1.0',
-    'depends': ['lms_core'],  
+    'depends': ['lms_core','website'],  
     'data': [
         'views/templates.xml',
         'security/ir.model.access.csv',
