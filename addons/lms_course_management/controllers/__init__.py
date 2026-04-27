3881c86168963008d1b50019483af4dfed8c0021
LordMeelo817 <131396490+LordMeelo817@users.noreply.github.com>
2026-04-26 19:04:34 -0500

    _init__.py
    Added CourseSearchController to handle course search requests.

--- addons/lms_course_management/controllers/__init__.py
+++ addons/lms_course_management/controllers/__init__.py
@@ -2,6 +2,7 @@
 from odoo import http
 from odoo.http import request
 
+
 class CourseCatalogController(http.Controller):
 
     @http.route('/lms/courses', type='http', auth='public', website=True)
@@ -9,3 +10,32 @@ class CourseCatalogController(http.Controller):
         return request.render(
             'lms_course_management.course_catalog_page'
         )
+
+
+class CourseSearchController(http.Controller):
+
+    @http.route('/lms/courses/search', type='json', auth='user', methods=['POST'])
+    def search_courses(self, query='', category=None, limit=20, **kwargs):
+        domain = []
+
+        if query:
+            domain += ['|', '|',
+                ('name', 'ilike', query),
+                ('description', 'ilike', query),
+                ('category', 'ilike', query),
+            ]
+
+        if category:
+            domain += [('category', '=', category)]
+
+        courses = request.env['lms.course'].search(domain, limit=limit)
+
+        return {
+            'count': len(courses),
+            'courses': courses.read([
+                'id',
+                'name',
+                'description',
+                'category'
+            ]),
+        }
